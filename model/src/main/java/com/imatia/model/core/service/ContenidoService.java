5d76741fac965a4db2f6556937acefac06da4a83
MargaritaLedo <margarita.ledo@campusdual.com>
2020-06-10 14:16:23 +0200

    se cambia las minusculas de los atributos del ContenidoDao y se cambia el pom generico en protertis en la etiqueta <database.url>

--- model/src/main/java/com/imatia/model/core/service/ContenidoService.java
+++ model/src/main/java/com/imatia/model/core/service/ContenidoService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.imatia.api.core.service.IContenidoService;
 import com.imatia.model.core.dao.ContenidoDao;
+import com.imatia.model.core.dao.GeneroDao;
 import com.ontimize.db.EntityResult;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
@@ -18,8 +19,11 @@ import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 public class ContenidoService implements IContenidoService{
 
  @Autowired private ContenidoDao contenidoDao;
+ @Autowired private GeneroDao generoDao;
  @Autowired private DefaultOntimizeDaoHelper daoHelper;
  
+ 
+ // contenido
  @Override
  public EntityResult contenidoQuery(Map<String, Object> keyMap, List<String> attrList)
    throws OntimizeJEERuntimeException {
@@ -40,5 +44,31 @@ public class ContenidoService implements IContenidoService{
  @Override
  public EntityResult contenidoDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
   return this.daoHelper.delete(this.contenidoDao, keyMap);
+ }
+ 
+ 
+ // Genero
+ 
+ 
+ @Override
+ public EntityResult generoQuery(Map<String, Object> keyMap, List<String> attrList)
+   throws OntimizeJEERuntimeException {
+  return this.daoHelper.query(this.generoDao, keyMap, attrList);
+ }
+
+ @Override
+ public EntityResult generoInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+  return this.daoHelper.insert(this.generoDao, attrMap);
+ }
+
+ @Override
+ public EntityResult generoUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+   throws OntimizeJEERuntimeException {
+  return this.daoHelper.update(this.generoDao, attrMap, keyMap);
+ }
+
+ @Override
+ public EntityResult generoDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+  return this.daoHelper.delete(this.generoDao, keyMap);
  }
 }
