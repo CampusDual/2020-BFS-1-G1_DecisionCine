47d07357c9d2cef165fed061811be6ea31bc60ac
Diego Silva Hermida <diego.silva@campusdual.com>
2020-06-10 14:25:50 +0200

    Refactorizar DAO noticia para usar convencion de espacios

--- model/src/main/java/com/imatia/model/core/dao/NoticiaDao.java
+++ model/src/main/java/com/imatia/model/core/dao/NoticiaDao.java
@@ -14,11 +14,11 @@ import org.springframework.stereotype.Repository;
 	configurationFilePlaceholder = "dao/placeholders.properties")
 public class NoticiaDao extends OntimizeJdbcDaoSupport {
 
-	public static final String NOTICIA_ID    		= "noticia_id";
-	public static final String NOTICIA_USUARIO_ID 	= "noticia_usuario_id";
+	public static final String NOTICIA_ID = "noticia_id";
+	public static final String NOTICIA_USUARIO_ID = "noticia_usuario_id";
 
-	public static final String DESCRIPCION   		= "noticia_descripcion";
-	public static final String FECHA_ALTA         	= "noticia_fecha_alta";
-	public static final String FECHA_MODIFICACION   = "noticia_fecha_modificacion";
+	public static final String DESCRIPCION = "noticia_descripcion";
+	public static final String FECHA_ALTA = "noticia_fecha_alta";
+	public static final String FECHA_MODIFICACION = "noticia_fecha_modificacion";
 
 }
