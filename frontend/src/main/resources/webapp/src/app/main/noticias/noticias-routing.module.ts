327847f783be418ed85b0b84295f62d9210eeefa
Diego Silva Hermida <diego.silva@campusdual.com>
2020-06-26 11:48:05 +0200

    improvement: añadimos una ruta en el módulo de noticias para cargar en componente noticias home

--- frontend/src/main/resources/webapp/src/app/main/noticias/noticias-routing.module.ts
+++ frontend/src/main/resources/webapp/src/app/main/noticias/noticias-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NoticiasHomeComponent } from './noticias-home/noticias-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: NoticiasHomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
