59edd7325375af5f717575aa5c187ac73f777ae2
Diego Silva Hermida <diego.silva@campusdual.com>
2020-06-26 11:41:51 +0200

    improvement: generamos el componente noticias home que mostrará la vista principal de noticias

---
+++ frontend/src/main/resources/webapp/src/app/main/noticias/noticias-home/noticias-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noticias-home',
+  templateUrl: './noticias-home.component.html',
+  styleUrls: ['./noticias-home.component.scss']
+})
+export class NoticiasHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
