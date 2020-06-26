8d58b4446525c7e07f0b85f9bf55d261dfdf9a0c
German <germancorbelle@campusdual.com>
2020-06-26 09:12:31 +0200

    creación del modulo y componente contenido. tambien esta añadido la opcion contenido en el menu lateral.

--- frontend/src/main/resources/webapp/src/app/main/main-routing.module.ts
+++ frontend/src/main/resources/webapp/src/app/main/main-routing.module.ts
@@ -4,9 +4,14 @@ import { AuthGuardService } from 'ontimize-web-ngx';
 
 import { MainComponent } from './main.component';
 import { HomeModule } from './home/home.module';
+import {ContenidoModule} from "./contenido/contenido.module";
 
 export function loadHomeModule() {
   return HomeModule;
+}
+
+export function loadContenidoModule() {
+  return ContenidoModule
 }
 
 export const routes: Routes = [
@@ -19,6 +24,10 @@ export const routes: Routes = [
       {
         path: 'home',
         loadChildren: loadHomeModule
+      },
+      {
+        path:  'contenido',
+        loadChildren: loadContenidoModule
       }
     ]
   }
