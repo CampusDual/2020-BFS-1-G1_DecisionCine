9dc1a2d695104fbe63beb0a2da4597e5d7c811a9
Diego Silva Hermida <diego.silva@campusdual.com>
2020-06-26 11:20:48 +0200

    improvement: generamos módulo noticias

---
+++ frontend/src/main/resources/webapp/src/app/main/noticias/noticias.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NoticiasRoutingModule } from './noticias-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NoticiasRoutingModule
+  ],
+  declarations: []
+})
+export class NoticiasModule { }
