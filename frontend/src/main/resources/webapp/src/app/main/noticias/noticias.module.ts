3b2ce68a4d4289bd57ac85738607a5e3e0ea32a1
Diego Silva Hermida <diego.silva@campusdual.com>
2020-06-26 11:36:49 +0200

    improvement: añadimos módulo ontimize web para poder usarlo en los componentes del módulo noticias

--- frontend/src/main/resources/webapp/src/app/main/noticias/noticias.module.ts
+++ frontend/src/main/resources/webapp/src/app/main/noticias/noticias.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NoticiasRoutingModule } from './noticias-routing.module';
+import { OntimizeWebModule } from 'ontimize-web-ngx';
 
 @NgModule({
   imports: [
     CommonModule,
+    OntimizeWebModule,
     NoticiasRoutingModule
   ],
   declarations: []
