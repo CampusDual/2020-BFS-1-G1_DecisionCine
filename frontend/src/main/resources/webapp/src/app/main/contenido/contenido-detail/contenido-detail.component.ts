edb6687ccb8a1aed9bbdf46b3ff3d392043dc4f3
Diego Silva Hermida <diego.silva@campusdual.com>
2020-06-30 12:30:12 +0200

    improvement: generar componente para mostrar detalle de contenido

---
+++ frontend/src/main/resources/webapp/src/app/main/contenido/contenido-detail/contenido-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contenido-detail',
+  templateUrl: './contenido-detail.component.html',
+  styleUrls: ['./contenido-detail.component.scss']
+})
+export class ContenidoDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
