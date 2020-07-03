06339be1c98dd51d987e05a51b0cd077fd38d52d
MargaritaLedo <margarita.ledo@campusdual.com>
2020-07-03 11:59:58 +0200

    probando filtro que no funciona al filtrar

--- frontend/src/main/resources/webapp/src/app/main/contenido/contenido-home/contenido-home.component.ts
+++ frontend/src/main/resources/webapp/src/app/main/contenido/contenido-home/contenido-home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FilterExpressionUtils, IExpression } from 'ontimize-web-ngx';
+
 
 @Component({
   selector: 'app-contenido-home',
@@ -8,11 +10,34 @@ import { Component, OnInit } from '@angular/core';
 export class ContenidoHomeComponent implements OnInit {
 
 public UrlFoto:string = "https://picsum.photos/200/300";
+    
+   
+	public  createFilter(values: Array<{ attr, value }>): IExpression {
+    // Prepare simple expressions from the filter components values
+    let filters: Array<IExpression> = [];
+    values.forEach(fil => {
+      if (fil.value) {
+        if (fil.attr === 'contenido_titulo' || fil.attr === 'contenido_duracion') {
+          filters.push(FilterExpressionUtils.buildExpressionLike(fil.attr, fil.value));
+        }
+        if (fil.attr === 'contenido_id') {
+          filters.push(FilterExpressionUtils.buildExpressionEquals(fil.attr, fil.value));
+        }
+      }
+    });
 
-
-
+    // Build complex expression
+   if (filters.length > 0) {
+      return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
+    } else {
+      return null;
+    }
+  }
+//
   constructor() { }
 
   ngOnInit() {
   }
+
+
 }
