2f74950f30d3ce4754f08457a788a74f6707096c
Diego Silva Hermida <diego.silva@campusdual.com>
2020-07-03 09:32:17 +0200

    improvement: establecer nueva ruta para el detalle usando wildcard para pasarle al detalle el id del contenido

--- frontend/src/main/resources/webapp/src/app/main/contenido/contenido-routing.module.ts
+++ frontend/src/main/resources/webapp/src/app/main/contenido/contenido-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ContenidoHomeComponent} from "./contenido-home/contenido-home.component";
+import { ContenidoDetailComponent } from './contenido-detail/contenido-detail.component';
 
 const routes: Routes = [{
   path: '',
   component: ContenidoHomeComponent
+}, 
+{  
+  path:':CONTENIDOID',
+  component: ContenidoDetailComponent
 }];
 
 @NgModule({
