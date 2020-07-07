e7429394dc074a738d4f2d5c419fd03af8959bf9
MargaritaLedo <margarita.ledo@campusdual.com>
2020-07-07 17:05:11 +0200

    se convierte en el grid en tabla y se siguen probando filtros pero no funcionan

--- frontend/src/main/resources/webapp/src/app/main/contenido/contenido-home/contenido-home.component.ts
+++ frontend/src/main/resources/webapp/src/app/main/contenido/contenido-home/contenido-home.component.ts
@@ -1,5 +1,5 @@
-import { Component,OnInit } from '@angular/core';
-import { FilterExpressionUtils, IExpression } from 'ontimize-web-ngx';
+import { Component,OnInit, ViewChild } from '@angular/core';
+import { FilterExpressionUtils, IExpression, OComboComponent } from 'ontimize-web-ngx';
 
 
 @Component({
@@ -9,34 +9,37 @@ import { FilterExpressionUtils, IExpression } from 'ontimize-web-ngx';
 })
 export class ContenidoHomeComponent implements OnInit {
 
-//public UrlFoto:string = "https://picsum.photos/200/300";
-    
-   
-	public  createFilter(values: Array<{ attr, value }>): IExpression {
+public createFilter(values: Array<{ attr: string, value: any }>): IExpression {
     // Prepare simple expressions from the filter components values
-    let filters: Array<IExpression> = [];
+	console.log('createFilter, start');
+	console.log(values);
+    const filters: IExpression[] = [];
     values.forEach(fil => {
       if (fil.value) {
-        if (fil.attr === 'contenido_titulo' || fil.attr === 'contenido_titulo') {
-          filters.push(FilterExpressionUtils.buildExpressionLike(fil.attr, fil.value));
+        if ( fil.attr === 'genero_id' ){   
+          filters.push(FilterExpressionUtils.buildExpressionLike('contenido_genero_id', fil.value));
+			//filters.push(FilterExpressionUtils.buildExpressionLike(fil.attr, 'El padrino'));
         }
-        if (fil.attr === 'contenido_id') {
-          filters.push(FilterExpressionUtils.buildExpressionEquals(fil.attr, fil.value));
-        }
+        
       }
     });
-
+	console.log(filters);
     // Build complex expression
-   if (filters.length > 0) {
+    if (filters.length > 0) {
+	console.log("entrando en if");
       return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
     } else {
+	console.log("entrando en else");
       return null;
     }
+
   }
+
 //
   constructor() { }
 
   ngOnInit() {
+
   }
 
 
