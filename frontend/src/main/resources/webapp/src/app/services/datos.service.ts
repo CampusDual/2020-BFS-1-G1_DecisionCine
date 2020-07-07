fabc7536d37d279e88dbf2e1ec1ae9bd90b418f6
Diego Silva Hermida <diego.silva@campusdual.com>
2020-07-07 18:01:53 +0200

    improvement: añadir servicio para gestionar peticiones personalizadas, en este caso contenido detalle necesita filtrar contenido por id

--- frontend/src/main/resources/webapp/src/app/services/datos.service.ts
+++ frontend/src/main/resources/webapp/src/app/services/datos.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CONFIG } from 'app/app.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatosService {
 
-  constructor() { }
+  httOptions = {
+    headers: new HttpHeaders({
+      'Authorization': 'Basic ZGVtbzpkZW1vdXNlcg=='
+    })
+  }
+
+  private apiEndPointContenido = CONFIG.apiEndpoint + "/contenidos/contenido/search";
+
+  constructor(private http: HttpClient) { }
+
+  getContentById(content_id: Number): Observable<RespuestaBackOntimize> {
+    const postBody = {
+      "filter": {
+        "contenido_id": +content_id
+      },
+      "columns": [
+        "contenido_id", "contenido_titulo", "contenido_sinopsis", "contenido_foto"
+      ]
+    }
+    return this.http.post<any>(this.apiEndPointContenido, postBody, this.httOptions);
+  }
+}
+
+interface RespuestaBackOntimize{
+
+  code: number;
+  data: Array<Object>;
+  message: '';
+  sqlTypes?: Object;
+  startRecordIndex?: number;
+  totalQueryRecordsNumber?: number;
+
 }
