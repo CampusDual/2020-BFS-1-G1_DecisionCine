2888a4cc3626abb039e55ba99f47874c4fd43c79
Diego Silva Hermida <diego.silva@campusdual.com>
2020-07-08 13:58:14 +0200

    refactor: importamos el módulo shared en el módulo de contendio para poder usar el componente de detalle en el módulo de contenido

--- frontend/src/main/resources/webapp/src/app/main/contenido/contenido.module.ts
+++ frontend/src/main/resources/webapp/src/app/main/contenido/contenido.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ContenidoRoutingModule } from './contenido-routing.module';
 import {OntimizeWebModule} from "ontimize-web-ngx";
 import {ContenidoHomeComponent} from "./contenido-home/contenido-home.component";
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     OntimizeWebModule,
-    ContenidoRoutingModule
+    ContenidoRoutingModule,
+    SharedModule
   ],
   declarations: [
     ContenidoHomeComponent
