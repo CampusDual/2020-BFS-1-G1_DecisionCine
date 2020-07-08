86a1c9333d61885d86dbc9c45d33f57f53ed4ee0
Diego Silva Hermida <diego.silva@campusdual.com>
2020-07-08 13:47:26 +0200

    refactor: declarar y exportar el componente de detalle de contenido en el módulo shared

--- frontend/src/main/resources/webapp/src/app/shared/shared.module.ts
+++ frontend/src/main/resources/webapp/src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { CommonModule } from '@angular/common';
 import { SharedComponentFooterComponent } from "./shared-component-footer/shared-component-footer.component";
+import { ContenidoDetailComponent } from './contenido-detail/contenido-detail.component';
 
 @NgModule({
   imports: [
     OntimizeWebModule
   ],
   declarations: [
-	SharedComponentFooterComponent
+    SharedComponentFooterComponent,
+    ContenidoDetailComponent
   ],
   exports: [
     CommonModule,
-SharedComponentFooterComponent
+    SharedComponentFooterComponent,
+    ContenidoDetailComponent
   ]
 })
 export class SharedModule { }
