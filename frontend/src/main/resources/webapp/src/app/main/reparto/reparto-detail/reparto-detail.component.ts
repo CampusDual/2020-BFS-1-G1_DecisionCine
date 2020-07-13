fa771a24cc979a06912400db819da099096138b5
MargaritaLedo <margarita.ledo@campusdual.com>
2020-07-13 10:25:52 +0200

    se crea unicamente el componente pendiente de enroutarlo

---
+++ frontend/src/main/resources/webapp/src/app/main/reparto/reparto-detail/reparto-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reparto-detail',
+  templateUrl: './reparto-detail.component.html',
+  styleUrls: ['./reparto-detail.component.scss']
+})
+export class RepartoDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
