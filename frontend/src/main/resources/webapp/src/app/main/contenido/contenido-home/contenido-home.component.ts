eb58862790d633c928701edcd0daf01091c9f606
Diego Silva Hermida <diego.silva@campusdual.com>
2020-07-15 13:18:07 +0200

    refactor: casi está listo el filtro revisarlo

--- frontend/src/main/resources/webapp/src/app/main/contenido/contenido-home/contenido-home.component.ts
+++ frontend/src/main/resources/webapp/src/app/main/contenido/contenido-home/contenido-home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FilterExpressionUtils, IExpression, OComboComponent } from 'ontimize-web-ngx';
+import { FilterExpressionUtils, IExpression, OComboComponent, OValueChangeEvent, OGridComponent } from 'ontimize-web-ngx';
 
 
 @Component({
@@ -9,38 +9,66 @@ import { FilterExpressionUtils, IExpression, OComboComponent } from 'ontimize-we
 })
 export class ContenidoHomeComponent implements OnInit {
 
+  private idComboSelected: Number;
+
+  @ViewChild('grid')
+  protected contenidoGrid   : OGridComponent;
+
   public createFilter(values: Array<{ attr: string, value: any }>): IExpression {
     // Prepare simple expressions from the filter components values
     console.log('createFilter, start');
     console.log(values);
     const filters: IExpression[] = [];
-    values.forEach(fil => {
-      if (fil.value) {
-        if (fil.attr === 'genero_id') {
-          filters.push(FilterExpressionUtils.buildExpressionEquals('contenido_genero_id', fil.value));
-          //filters.push(FilterExpressionUtils.buildExpressionLike(fil.attr, 'El padrino'));
-        }
 
-      }
-    });
+    filters.push(FilterExpressionUtils.buildExpressionEquals('contenido_genero_id', this.idComboSelected));
+
+    console.log(filters);
+
+    // values.forEach(fil => {
+    //   if (fil.value) {
+    //     if (fil.attr === 'genero_id') {
+
+    //       console.log("añadiendo un filtro");
+    //       filters.push(FilterExpressionUtils.buildExpressionEquals('contenido_genero_id', this.idComboSelected));
+    //       //filters.push(FilterExpressionUtils.buildExpressionLike(fil.attr, 'El padrino'));
+
+    //     } else {
+
+    //       console.log("el atributo NO es genero_id, NO se añade un filtro");
+
+    //     }
+
+    //   } else {
+
+    //     console.log("no hay valor");
+
+    //   }
+    // });
     console.log(filters);
     // Build complex expression
     if (filters.length > 0) {
-      console.log("entrando en if");
+      console.log("SI hay al menos un filtro para reducir");
       return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
     } else {
-      console.log("entrando en else");
+      console.log("NO hay al menos un filtro para reducir");
       return null;
     }
 
   }
 
-  //
+  valorCombo(event: OValueChangeEvent) {
+
+    console.log("entrando en: valorCombo");
+    console.log(event);
+    console.log(event.newValue);
+    this.idComboSelected = event.newValue;
+
+  }
+
   constructor() { }
 
   ngOnInit() {
 
   }
-
 
 }
