1bb5cad8296dc3d16ffc2befffb858b09a74c788
German <germancorbelle@campusdual.com>
2020-07-15 17:33:59 +0200

    cambiar combre contenido a reparto

--- frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
+++ frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
@@ -3,6 +3,6 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: '', route: '/main/home' },
   { id: 'noticias', name: 'NEWS', icon: '', route: '/main/noticias' },
-  { id: 'contenido', name: 'Contenido', icon: '', route: '/main/contenido' },
+  { id: 'contenido', name: 'Cartelera', icon: '', route: '/main/contenido' },
   { id: 'reparto', name: 'Reparto', icon: '', route: '/main/reparto' }
 ];
