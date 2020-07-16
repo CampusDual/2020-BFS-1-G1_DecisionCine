3092c5a180a1d2ffe361366b393f4a79142cb04c
Diego Silva Hermida <diego.silva@campusdual.com>
2020-07-16 12:49:42 +0200

    refactor: mostrar filmografía en el detalle de reparto, relacionada con el detalle reparto, TODO ampliar div para ver titulo correctamente

--- frontend/src/main/resources/webapp/src/app/main/reparto/reparto-detail/reparto-detail.component.ts
+++ frontend/src/main/resources/webapp/src/app/main/reparto/reparto-detail/reparto-detail.component.ts
@@ -4,57 +4,47 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { OGridItemDirective } from 'ontimize-web-ngx/ontimize/components/grid/grid-item/o-grid-item.directive';
 
 @Component({
-  selector: 'app-reparto-detail',
-  templateUrl: './reparto-detail.component.html',
-  styleUrls: ['./reparto-detail.component.scss']
+	selector: 'app-reparto-detail',
+	templateUrl: './reparto-detail.component.html',
+	styleUrls: ['./reparto-detail.component.scss']
 })
 export class RepartoDetailComponent implements OnInit {
 
-public repartoFiltrado;
-public repartoContenidoFiltrado;
-public mostrarContenido: boolean;
+	public repartoFiltrado;
+	public contenidoFiltradoPorReparto;
+	public mostrarFilmografia: boolean;
 
-  constructor(activatedRoute: ActivatedRoute, dataService: DatosService,private router: Router,
+	constructor(activatedRoute: ActivatedRoute, dataService: DatosService, private router: Router,
 		private actRoute: ActivatedRoute) {
-    activatedRoute.params.subscribe(params => {
-      dataService.getRepartoById(params["REPARTOID"]).subscribe(response => {
-        console.log(response);
-        this.repartoFiltrado = response["data"][0];
-      })
-      dataService.getRepartoByContentId(params["CONTENIDOID"]).subscribe(response => {
+		activatedRoute.params.subscribe(params => {
+			dataService.getRepartoById(params["REPARTOID"]).subscribe(response => {
 				console.log(response);
-				this.repartoContenidoFiltrado = response["data"];
-				this.inicializarContenido(this.repartoContenidoFiltrado.length);
+				this.repartoFiltrado = response["data"][0];
 			})
-    })
+			dataService.getContentByRepartoId(params["REPARTOID"]).subscribe(response => {
+				console.log(response);
+				this.contenidoFiltradoPorReparto = response["data"];
+				this.inicializarContenido(this.contenidoFiltradoPorReparto.length);
+			})
+		})
 
-  }
-  ngOnInit() {
-  }
+	}
+	ngOnInit() {
+	}
 
-  navigate() {
+	navigate() {
 		this.router.navigate(['../', 'login'], { relativeTo: this.actRoute });
 	}
-	
-	onRedirectToDetail(event: OGridItemDirective) {
-	let idDetalle: Number;
-	console.log("entrando en: onRedirectToDetail() detalles Contendido");
-	console.log(event);
-	console.log(event.getItemData());
-	console.log(event.getItemData()['contenido_id']);
-	idDetalle = event.getItemData()['contenido_id'];
-	this.router.navigateByUrl("/main/contenido/" + idDetalle);
-	}
 
 
-  inicializarContenido(cantidad:number){
-		console.log("entrando en inicializarContenido: " + cantidad);
-		if(cantidad==undefined){
-			console.log ("no hay contenido");
-			this.mostrarContenido=false;
-		}else {
-			console.log ("hay: "+ cantidad);
-			this.mostrarContenido=true;
+	inicializarContenido(cantidad: number) {
+		console.log("entrando en inicializarContenido de detalle reparto: " + cantidad);
+		if (cantidad == undefined) {
+			console.log("NO hay filmografía relacionada con este reparto");
+			this.mostrarFilmografia = false;
+		} else {
+			console.log("SI hay: " + cantidad + " filmografías relacionadas con este reparto");
+			this.mostrarFilmografia = true;
 		}
 	}
 }
